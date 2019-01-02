2f2762057238bdb183520e8d15491df07308e746
Pooja Kumari <thisispooja97@gmail.com>
2019-01-02 21:00:35 +0530

    Update 3b.cpp

--- 3b.cpp
+++ 3b.cpp
@@ -1,30 +1,34 @@
 #include<iostream>
 using namespace std;
-class Complex {
-        private:
-                int re;
-                int im;
-        public:
-                Complex(int a, int b):re(a),im(b){}
-		void print();
-		void operator++(int);
-		void operator++();
+class counter
+{
+private:
+int count;
+public:
+counter()
+{
+count=0;
+}
+int getdata()
+{
+return count;
+}
+void operator++()
+{
+++count;
+}
+void operator++(int=0)
+{
+count++;
+}
 };
-void Complex::print() { 
-	cout<<re<<"+ i"<<im<<endl;
-}
-void Complex::operator++(int) {
-       	cout<<"Post Increment : "<<re++<<"+ i"<<im++<<endl;
-}
-void Complex::operator++() { 
-	cout<<"Pre Increment : "<<++re<<" +i"<<++im<<endl;
-}
-int main() {
-	int x, y;
-	cout<<"\nEnter real and imaginary values for object : ";
-	cin>>x>>y;
-	Complex a(x,y);
-	a++;
-	++a;
-	return 0;
+int main()
+{
+counter c1,c2;
+++c1;
+c2++;
+c2++;
+cout<<c1.getdata();
+cout<<c2.getdata();
+return 0;
 }
